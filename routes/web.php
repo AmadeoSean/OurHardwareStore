bd28e8d50acd0f8a4ef76a8a57ad61dd1a02297e
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2023-09-02 16:25:58 +0700

    routing public

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,78 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/#/en/back', function () {
     return view('admin');
 });
 Route::get('/id', function () {
     return view('public');
+});
+
+#public
+
+#https://www.mrdiy.com/id/page/tentang-kami/
+Route::get('tentang-kami', function(){
+    return 'ini halaman about us atau tentang kami';
+});
+
+#https://www.mrdiy.com/id/page/pencapaian-kami/
+Route::get('pencapaian-kami/', function(){
+    return 'ini halaman our achievements atau pencapaian kami';
+});
+
+#https://www.mrdiy.com/id/page/penghargaan/
+Route::get('penghargaan', function(){
+    return 'ini halaman award atau penghargaan';
+});
+
+#https://www.mrdiy.com/id/page/merek-mrdiy/
+Route::get('merek-OurHardwareStore', function(){
+    return 'ini halaman penjelasan merek OurHardwareStore';
+});
+
+#https://www.mrdiy.com/id/career/
+Route::get('career', function(){
+    return 'ini halaman lowongan kerja';
+});
+
+#https://www.mrdiy.com/id/page/kontak-kami/
+Route::get('contact-us', function(){
+    return 'ini halaman kontak kami';
+});
+
+#https://www.mrdiy.com/id/storeopenings/
+Route::get('storeopenings', function(){
+    return 'ini halaman berita toko OurHardwareStore yang baru dibuka';
+});
+
+#https://www.mrdiy.com/id/event/
+Route::get('event', function(){
+    return 'ini halaman acara yang dibuat OurHardwareStore';
+});
+
+#https://www.mrdiy.com/id/pressrelease/
+Route::get('pressrelease', function(){
+    return 'ini halaman berita siaran pers dari toko OurHardwareStore';
+});
+
+#https://www.mrdiy.com/id/promotion/
+Route::get('promotion', function(){
+    return 'ini halaman promosi yang berlangsung pada toko OurHardwareStore';
+});
+
+#https://www.mrdiy.com/id/page/produk-kami/
+Route::get('produk', function(){
+    return 'ini halaman kategori produk barang pada toko OurHardwareStore';
+});
+
+Route::get('kategori/{id}', function($id){
+    return 'ini halaman produk barang yang berkategori '. $id. ' pada toko OurHardwareStore ';
+});
+
+Route::get('produk/{id}', function($id){
+    return 'ini halaman produk barang '. $id. ' pada toko OurHardwareStore ';
 });
